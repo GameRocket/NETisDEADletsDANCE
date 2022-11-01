5b6e4dea05aa6844187c762eb8203f1ad0252504
rengetsu <rengetsu@mail.ru>
2022-11-01 21:11:47 +0200

    Change PlayerController and PlayerMotor code

--- Assets/Scripts/PlayerContoller.cs
+++ Assets/Scripts/PlayerContoller.cs
@@ -1,19 +1,39 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Than we add a PlayerController it add a PlayerMotor which add a Rigidbody
+/// </summary>
 [RequireComponent(typeof(PlayerMotor))]
 public class PlayerContoller : MonoBehaviour
 {
-    // Start is called before the first frame update
+    /// <summary>
+    /// Player speed
+    /// </summary>
+    [SerializeField]
+    private float speed = 5f;
+
+    /// <summary>
+    /// Player motor reference
+    /// </summary>
+    private PlayerMotor motor;
+
+    /// <summary>
+    /// After player start movements
+    /// </summary>
     void Start()
     {
-        
+        motor = GetComponent<PlayerMotor>();
     }
 
-    // Update is called once per frame
+    /// <summary>
+    /// Calculate movement velocity as a 3D Vector
+    /// </summary>
     void Update()
     {
-        
+        //  Horizontal movement
+        float _xMov = Input.GetAxisRaw("Horizotal");
+
+        //  Vertical movement
+        float _zMov = Input.GetAxisRaw("Vertical");
     }
 }
