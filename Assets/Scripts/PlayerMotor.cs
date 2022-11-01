f122e355a4b9b0893d79c8f5512c43030671cd91
rengetsu <rengetsu@mail.ru>
2022-11-01 21:31:31 +0200

    New methods in PlayerMotor

--- Assets/Scripts/PlayerMotor.cs
+++ Assets/Scripts/PlayerMotor.cs
@@ -4,5 +4,38 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerMotor : MonoBehaviour
 {
+    private Vector3 velocity = Vector3.zero;
 
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    /// <summary>
+    /// Gets a movement vector from player controller
+    /// </summary>
+    /// <param name="_velocity"></param>
+    public void Move(Vector3 _velocity)
+    {
+        velocity = _velocity;
+    }
+
+    /// <summary>
+    /// Run every physics iteration
+    /// </summary>
+    void FixedUpdate()
+    {
+        PerformMovement();
+    }
+
+    //  Perform movement based on velocity variable
+    void PerformMovement()
+    {
+        if ( velocity != Vector3.zero )
+        {
+            rb.MovePosition();
+        }
+    }
 }
