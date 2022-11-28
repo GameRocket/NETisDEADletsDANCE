3c62223d592d58f48b70f12f0bb2c9aa033401a1
rengetsu <rengetsu@mail.ru>
2022-11-28 02:16:01 +0200

    Add camera reference to playershoot class

--- Assets/Scripts/PlayerShoot.cs
+++ Assets/Scripts/PlayerShoot.cs
@@ -4,5 +4,22 @@ using UnityEngine;
 
 public class PlayerShoot : MonoBehaviour
 {
+    /// <summary>
+    /// Reference to our player camera
+    /// </summary>
+    [SerializeFiled]
+    private Camera cam;
 
+    /// <summary>
+    /// Checking after start
+    /// </summary>
+    void Start()
+    {
+        //  Check if camera inserted into camera slot
+        if (cam == null)
+        {
+            Debug.LogError("PlayerShoot: No camera referenced!");
+            this.enabled = false;
+        }
+    }
 }
