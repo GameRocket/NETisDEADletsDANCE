4451494732d64ca7456197ffdee171870ad7bfc9
rengetsu <rengetsu@mail.ru>
2022-11-30 01:11:22 +0200

    Create Raycast hiting for shooting

--- Assets/Scripts/PlayerShoot.cs
+++ Assets/Scripts/PlayerShoot.cs
@@ -1,14 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerShoot : MonoBehaviour
 {
     /// <summary>
+    /// Reference to our weapon class
+    /// </summary>
+    public PlayerWeapon weapon;
+
+    /// <summary>
     /// Reference to our player camera
     /// </summary>
     [SerializeField]
     private Camera cam;
+
+    /// <summary>
+    /// Allows to control that we hit in Unity
+    /// </summary>
+    [SerializeField]
+    private LayerMask mask;
 
     /// <summary>
     /// Checking after start
@@ -28,7 +37,21 @@ public class PlayerShoot : MonoBehaviour
         //  Check if we get some input
         if (Input.GetButtonDown("Fire1"))
         {
+            Shoot();
+        }
+    }
 
+    /// <summary>
+    /// In this method we will do RayCast
+    /// </summary>
+    void Shoot()
+    {
+        RaycastHit _hit;
+
+        //  Checking if we hit something
+        if( Physics.Raycast(cam.transform.position, cam.transform.forward, out _hit, weapon.range, mask) )
+        {
+            Debug.Log("We hit " + _hit.collider.name);
         }
     }
 }
--- Assets/Scripts/PlayerWeapon.cs
+++ Assets/Scripts/PlayerWeapon.cs
@@ -1,9 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Class for weapon variables
+/// </summary>
 [System.Serializable]
-public class PlayerWeapon : MonoBehaviour
+public class PlayerWeapon
 {
     /// <summary>
     /// Name of our weapon
