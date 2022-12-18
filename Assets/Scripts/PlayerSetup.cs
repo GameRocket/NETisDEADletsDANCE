3b7a0ce374b3fdc28a56862cfba2f7aa73ad531b
rengetsu <rengetsu@mail.ru>
2022-12-18 23:18:45 +0200

    PlayerSetup update

--- Assets/Scripts/PlayerSetup.cs
+++ Assets/Scripts/PlayerSetup.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class PlayerSetup : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Behaviour[] componentsToDisable;
+
+    bool isLocalPlayer = false;
+
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if(!isLocalPlayer)
+        {
+            for ( int i = 0; i < componentsToDisable.Length; i++ )
+            {
+                componentsToDisable[i].enabled = false;
+            }
+        }
     }
 }
